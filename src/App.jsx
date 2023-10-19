00442531a4801fe1ddec0214436aca67078398c9
NikitaKx9 <nikitakx9@gmail.com>
2023-10-19 18:59:53 +0300

    FC CC done

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+import { Component } from "react";
+
+import ShopItemClass from "./components/StoreCC/StoreCC";
+import ShopItemFunc from "./components/StoreFC/StoreFC";
+
+const itemData = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+export default function App() {
+  return (
+    <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemClass item={itemData} />;
+      <ShopItemFunc item={itemData} />
+    </div>
+  </div>
+    // <div className="App">
+    //   <ShopItemClass item={itemData} />;
+    //   <ShopItemFunc item={itemData} />
+    // </div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
